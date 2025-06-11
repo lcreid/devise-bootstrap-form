7f9b169eb41f6acc8e9b9033784e905e148847df
Larry Reid <lcreid@jadesystems.ca>
2025-06-11 00:46:37 +0000

    Add home_controller

--- test/rails_app_8.0/config/routes.rb
+++ test/rails_app_8.0/config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
